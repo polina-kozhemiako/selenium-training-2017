09c4f98d578147038337dfc166fa794e89815a12
polina-kozhemiako <polinakozhemyako@gmail.com>
2017-11-01 13:51:29 +0200

    Edited according to the lector's comments.

--- src/test/java/Task3.java
+++ src/test/java/Task3.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.List;
 
@@ -17,41 +16,41 @@ import java.util.List;
  * Created by polina.kozhemiako on 10/25/2017.
  */
 public class Task3 {
+    private static WebDriver driver;
 
     @Before
     public void start(){
         FirefoxDriverManager.getInstance().setup();
+        driver = new FirefoxDriver();
     }
 
-
     @Test
-    public void VerifyHeadersForPages(){
-        WebDriver driver = new FirefoxDriver();
+    public void verifyHeadersArePresentOnPages(){
         driver.get("http://127.0.0.1:8080/litecart/admin");
         // log in
-        driver.findElement(By.cssSelector("input[name=username]")).sendKeys("admin");
+        driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
-        driver.findElement(By.xpath(".//*[@id='box-login']/form/div[2]/button")).click();
+        driver.findElement(By.name("login")).click();
         // get menu items
         List<WebElement> menuItems = driver.findElements(By.cssSelector("#app-"));
         for(int i=0; i < menuItems.size(); i++){
-            WebElement menuItem = driver.findElements(By.cssSelector("#app-")).get(i);
-            menuItem.click();
+            driver.findElement(By.xpath("//*[@id='app-']["+(i+1)+"]")).click();
+            Boolean isHeaderPresent = driver.findElements(By.cssSelector("#content h1")).size()>0;
+
+            Assert.assertTrue("H1 header is not present on the page.", isHeaderPresent);
             // get subitems
-            List<WebElement> subItems = driver.findElements(By.cssSelector("#app- ul[class=docs] li"));
-            if (subItems.size()>0)
-            {
-                for (int j=0; j < subItems.size(); j++) {
-                    WebElement subItem = driver.findElements(By.cssSelector("#app- ul[class=docs] li")).get(j);
-                    subItem.click();
-                    Boolean isHeaderPresent = driver.findElements(By.cssSelector("#content h1")).size()>0;
+            List<WebElement> subItems = driver.findElements(By.cssSelector("#app- ul.docs li"));
+            for (int j=0; j < subItems.size(); j++) {
+                    driver.findElements(By.cssSelector("#app- ul.docs li")).get(j).click();
+                    isHeaderPresent = driver.findElements(By.cssSelector("#content h1")).size()>0;
 
                     Assert.assertTrue("H1 header is not present on the page.", isHeaderPresent);
-                }
             }
         }
-
-        driver.close();
     }
 
+    @After
+    public void stop(){
+        driver.close();
+    }
 }
